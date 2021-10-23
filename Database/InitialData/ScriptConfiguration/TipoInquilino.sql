aa6b10cd579afef379def075db99e9efcbda19cb
Gamboa Porras <Gamboa Porras@GAMBOAPORRAS>
2021-10-23 14:02:33 -0600

    Add project files.

---
+++ Database/InitialData/ScriptConfiguration/TipoInquilino.sql
@@ -0,0 +1,43 @@
+﻿DROP TABLE IF EXISTS #TipoInquilinoTemp
+
+SELECT 
+Id_TipoInquilino, Descripcion, Estado INTO #TipoInquilinoTemp
+FROM (
+VALUES
+(1, 'FISICO',1), 
+(2, 'JURIDICO',1) 
+
+
+)AS TEMP (Id_TipoInquilino, Descripcion, Estado)
+
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Descripcion= TM.Descripcion, 
+   P.Estado= TM.Estado
+
+FROM dbo.TipoInquilino P
+INNER JOIN #TipoInquilinoTemp TM
+    ON P.Id_TipoInquilino= TM.Id_TipoInquilino
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO dbo.TipoInquilino(
+Id_TipoInquilino, Descripcion, Estado)
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM #TipoInquilinoTemp
+
+
+EXCEPT
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM dbo.TipoInquilino
+
+
+SET IDENTITY_INSERT dbo.TipoInquilino OFF
+
+GO
